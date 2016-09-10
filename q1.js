299e9543d676f6071e4cf2cac45a07e32bdb6ba0
rsstdd <rosstod@gmail.com>
2016-09-10 10:11:11 -0700

    validated html and css

--- q1.js
+++ q1.js
@@ -1,106 +1,136 @@
 (function() {
   'use strict';
-
-  $('.carousel.carousel-slider').carousel({full_width: true});
-  $('select').material_select();
-  $('.button-collapse').sideNav();
-
-
-
-  $('#source').on('blur', validate);
-  $('button').on('click', validate);
-  $('#upliftingBtn').on('click', validate);
-
-  $('.tooltipped').tooltip({delay: 50});
-
-
-  var $newsArticles = [];
-  var $upliftingArticles = [];
-
+  let $newsArticles = [];
+  let $upliftingArticles = [];
 
   //Will return an array of all the top headlines from selected source//
-//associated-press
 function validate(event) {
-  var $val = $('#source').val();
-  var $target = $(event.target);
-  var $articlerow = $('#article-row');
+  let $val = $('#source').val();
+  let $target = $(event.target);
+  let $articlerow = $('#article-row');
 
   if ($val === 'select') {
-    Materialize.toast('Select An Agency', 2000);
+    Materialize.toast('Select An Agency', 5000);
     $('#source').addClass('invalid');
     return;
-  } else if ($val === 'uplifting-news') {
+    } else {
 
-    var $xhr = $.getJSON('https://www.reddit.com/r/UpliftingNews.json');
+    let $xhr = $.getJSON(`https://newsapi.org/v1/articles?source=${$val}&sortBy=top&apiKey=7ac8963fbab04b74bf07af53f91e3d0d`);
     $xhr.done(function(data) {
       if ($xhr.status !== 200) {
           return;
       }
-      var $upliftingArticles = [];
-
-      //$data is an array of the Articles
-      var $data = data.data.children;
-      console.log($data);
-      var image = data.data.children[0].data.preview.images[0].source;//this finds the image
-      console.log(image);
-      $data.map(function(article) {
-        $upliftingArticles.push(article);
-       });
-       //console.log(upliftingArticles.preview.images.source.url);
-    });
-  } else {
-    var $xhr = $.getJSON(`https://newsapi.org/v1/articles?source=${$val}&sortBy=top&apiKey=7ac8963fbab04b74bf07af53f91e3d0d`);
-
-      $xhr.done(function(data) {
-        if ($xhr.status !== 200) {
-            return;
-        }
-        $newsArticles =[];
-        data.articles.map(function(article) {
-            $newsArticles.push(article);
-        });
-        render();
+      $newsArticles =[];
+      data.articles.map(function(article) {
+          $newsArticles.push(article);
       });
-      event.preventDefault();
+      render();
+    });
     }
+    event.preventDefault();
   }
+
+  function validateUplifting(event) {
+     let $val = $('#source').val();
+     let $target = $(event.target);
+     let $articlerow = $('#article-row');
+     const $xhr = $.getJSON('https://www.reddit.com/r/UpliftingNews.json');
+
+     $xhr.done(function(data) {
+         if ($xhr.status !== 200) {
+             return;
+         }
+         data.data.children.map(function(article) {
+           $upliftingArticles.push(article);
+          });
+          renderUplifting();
+        });
+         event.preventDefault();
+  }
+
+  function renderUplifting() {
+    $('#article-row').empty();
+    for (let article of $upliftingArticles) {
+      let $col = $('<div class="col s12 m6">');
+      let $cardDiv = $('<div class="card small">');
+      let $card = $('<div class="card-image waves-effect waves-block waves-light">');
+      $('#article-row').append($col);
+      $col.append($cardDiv);
+      $cardDiv.append($card);
+
+      let $upliftImg = $('<img class="activator">');
+      $upliftImg.attr({
+        src: article.data.preview.images[0].source.url,
+        alt: `${article.data.title} Image`,
+      }).height('350px');
+      $card.append($upliftImg);
+
+      let $cardContent = $('<div class="card-content center">');
+      let $titleSpan = $(`<p class="card-title activator grey-text text-darken-4 truncate "> ${article.data.title} </p><i class="material-icons right">more_vert</i>`);
+      let $read = $(`<p><a href="${article.data.url}" target="_blank">Read the Full article here</a></p>`);
+      $cardDiv.append($cardContent);
+      $cardContent.append($titleSpan);
+      $cardContent.append($read);
+
+      let $cardReveal = $('<div class="card-reveal">');
+      let $revealTitleSpan = ('<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>');
+      let $revealText = $(`<h5>${article.data.title}</h5>`);
+      $cardReveal.append($revealTitleSpan);
+      $cardDiv.append($cardReveal);
+      $cardReveal.append($revealText);
+      $cardReveal.append($read);
+       }
+    }
+
   function render() {
+    $('#article-row').empty();
+    for (const article of $newsArticles) {
+      //create col; append to row; create card; append to col//
+      let $col = $('<div class="col s12 m6">');
+      let $cardDiv = $('<div class="card small">');
+      let $card = $('<div class="card-image waves-effect waves-block waves-light">');
+      $('#article-row').append($col);
+      $col.append($cardDiv);
+      $cardDiv.append($card);
 
-    $('#articles').empty();
+      //create img; assign url and alt as attr; append to img/
+      let $img = $('<img class="activator src=">');
+      $img.attr({
+        src: article.urlToImage,
+        alt: `${article.title} Image`,
+      }).height('350px');
+      $card.append($img);
 
-    var $firstDiv = $('<div class="carousel carousel-slider center" data-indicators="true">');
-    $('#articles').append($firstDiv);
+      //create cardContent; create title; append to card//
+      //assign article title to titleSpan; append title//
+      let $cardContent = $('<div class="card-content center">');
+      $cardDiv.append($cardContent);
+      let $titleSpan = $(`<span class="card-title activator grey-text text-darken-4">${article.title}<i class="material-icons right">more_vert</i>`);
+      $cardContent.append($titleSpan);
+      //<a href="${article.url}" target="_blank">
+      //create link; assign attr; assign text; apped to cardContent//
+      let $link = $(`<p><a href="${article.url}" target="_blank">Read the full article here</a></p>`);
+      $cardContent.append($link);
 
-    for (var article of $newsArticles) {
-      var $carouselBtnDiv = $('<div class="carousel-fixed-item center">');//this is appending
-      var $carouselBtn = $('<a class="btn waves-effect white grey-text darken-text-2">Select</a>');
-      var $carouselImage = $('<img>');
-      var $carouselPanel = $('<div class="carousel-item red white-text">');
-      var $title = $('<h2>');
-      var $description = $('<p>');
+      //create cardRevealDiv; append to $card;
+      let $cardReveal = $('<div class="card-reveal">');
+      $cardDiv.append($cardReveal);
+      let $revealTitleSpan = ('<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>');
+      $cardReveal.append($revealTitleSpan);
 
-
-      $carouselBtn.text(article.title);
-      $carouselBtn.attr('href', article.url);
-      $carouselBtnDiv.append($carouselBtn);
-
-      $carouselImage.attr('src', article.urlToImage);
-      $carouselBtnDiv.append($carouselImage);
-      // $carouselImage.attr('src', article.urlToImage);
-      // $carouselPanel.attr('href', article.url);
-
-
-
-      $title.text(article.title);
-      $description.text(article.description);
-      $carouselPanel.append($title);
-      $carouselPanel.append($description);
-      $firstDiv.append($carouselPanel);
-      $firstDiv.append($carouselPanel);
-      $firstDiv.append($carouselBtnDiv);
-
-
+      let $articleTitle = $(`<h5>${article.title}</h5>`)
+      let $revealText = $(`<h6>${article.description}</h6>`);
+      // $revealText.text(article.description);
+      let $revealAuthor = $(`<p>${article.author}</a></p>`);
+      let $read = $(`<p><a href="${article.url}" target="_blank">Read the Full article here</a></p>`);
+      $cardReveal.append($articleTitle);
+      $cardReveal.append($revealText);
+        $cardReveal.append($revealAuthor);
+      $cardReveal.append($read);
     }
-
   }
+
+  $('select').material_select();
+  $('#newsBtn').on('click', validate);
+  $('#upLiftingBtn').on('click', validateUplifting);
 })();
